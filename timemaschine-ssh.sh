4b4ee52ae6204caa6baf27d3c0f2d2bf00be7174
Nikolay Vasilchuk <Anonym.tsk@gmail.com>
2014-11-07 15:27:53 +0300

    New version

--- timemaschine-ssh.sh
+++ timemaschine-ssh.sh
@@ -3,51 +3,97 @@
 ## READ THE CONFIG FILE
 source timemaschine-ssh.conf
 
-createTunnel() {
-    if [[ -n "$KEYFILE" && -e "$KEYFILE" ]]; then
-        REMOTE_LOGIN="-i $KEYFILE $REMOTE_USER@$REMOTE_HOST"
-    else
-        REMOTE_LOGIN="$REMOTE_USER@$REMOTE_HOST"
+SSH_COMMAND="ssh -gNf -L $LOCAL_AFP_PORT:$AFP_HOST:548"
+
+log() {
+    if [ "$QUIET" = "false" ]; then
+        echo "$1"
     fi
+}
 
-    if [ "$QUIET" = "false" ]; then echo "Connecting to server: $REMOTE_HOST" >&2; fi
+######### SSH tunnel methods #########
 
-    # Create tunnel to port 548 on remote host and make it avaliable at port $LOCAL_AFP_PORT at localhost
-    # Also tunnel ssh for connection testing purposes
-    ssh -gNf -L "$LOCAL_AFP_PORT:$AFP_HOST:548" -C "$REMOTE_LOGIN"
+getPidSshCommand() {
+    echo `ps aux | egrep -w "$SSH_COMMAND" | grep -v egrep | awk '{print $2}'`
+}
 
-    if [[ $? -eq 0 ]]; then
+startSshTunnel() {
+    log "Starting ssh tunnel to $REMOTE_HOST..."
+
+    local PID=`getPidSshCommand`
+    if [[ -n "$PID" ]]; then
+        log "Tunnel to $REMOTE_HOST already active"
+    else
+        if [[ -n "$KEYFILE" && -e "$KEYFILE" ]]; then
+            REMOTE_LOGIN="-i $KEYFILE $REMOTE_USER@$REMOTE_HOST"
+        else
+            REMOTE_LOGIN="$REMOTE_USER@$REMOTE_HOST"
+        fi
+
+        log "Connecting to server: $REMOTE_HOST..."
+
+        # Create tunnel
+        $SSH_COMMAND -C "$REMOTE_LOGIN"
+
+        if [[ $? -eq 0 ]]; then
+            log "Tunnel to $REMOTE_HOST created successfully"
+        else
+            log "An error occurred creating a tunnel to $REMOTE_HOST. RC was $?"
+            exit 1
+        fi
+    fi
+}
+
+stopSshTunnel() {
+    local PID=`getPidSshCommand`
+    if [[ -z "$PID" ]]; then
+        log "Tunnel to $REMOTE_HOST is not active"
+    else
+        kill "$PID"
+        log "Tunnel to $REMOTE_HOST killed successfully"
+    fi
+}
+
+######### End SSH tunnel methods #########
+
+######### dns-sd methods #########
+
+getPidDnsSdCommand() {
+    echo `ps aux | egrep -w "dns-sd -R $LABEL" | grep -v egrep | awk '{print $2}'`
+}
+
+startDnsSd() {
+    log "Starting dns-sd service..."
+
+    local PID=`getPidDnsSdCommand`
+    if [[ -n "$PID" ]]; then
+        log "Service already active"
+    else
         # Register AFP as service via dns-sd
-        dns-sd -R "$LABEL" _afpovertcp._tcp . "$LOCAL_AFP_PORT" > /dev/null &
+        dns-sd -R "$LABEL" _afpovertcp._tcp . $LOCAL_AFP_PORT > /dev/null &
 
-        if [ "$QUIET" = "false" ]; then echo "Tunnel to $REMOTE_HOST created successfully"; fi
-        exit 0
+        if [[ $? -eq 0 ]]; then
+            log "Service started successfully"
+        else
+            log "An error occurred starting dns-sd service. RC was $?"
+            exit 1
+        fi
+    fi
+}
+
+stopDnsSd() {
+    local PID=`getPidDnsSdCommand`
+    if [[ -z "$PID" ]]; then
+        log "Service dns-sd is not active"
     else
-        if [ "$QUIET" = "false" ]; then echo "An error occurred creating a tunnel to $REMOTE_HOST RC was $?"; fi
-        exit 1
+        kill "$PID"
+        log "Service dns-sd killed successfully"
     fi
 }
 
-killTunnel() {
-    MYPID=`getPid`
-    for i in $MYPID; do kill $i; done
-    if [ "$QUIET" = "false" ]; then echo "All processes killed"; fi
-}
+######### End dns-sd methods #########
 
-status() {
-    MYPID=`getPid`
-    if [[ -z "$MYPID" ]]; then
-        if [ "$QUIET" = "false" ]; then echo "Tunnel to $REMOTE_HOST is NOT ACTIVE"; fi
-        exit 0
-    fi
-    if [ "$QUIET" = "false" ]; then echo "Tunnel to $REMOTE_HOST is ACTIVE"; fi
-    exit 1
-}
-
-getPid() {
-    MYPID=`ps aux | egrep -w "$REMOTE_HOST|dns-sd -R $LABEL" | grep -v egrep | awk '{print $2}'`
-    echo $MYPID
-}
+######### Public methods #########
 
 help() {
     SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
@@ -62,41 +108,97 @@ Options
  -q, --quiet                 quiet mode
  -k, --kill                  kill all $SCRIPT_NAME processes
  -r, --restart               restart
+ -c, --check                 check service and restart if needed
  -h, --help                  show this screen
 "
-exit 0
+
+    exit 0
 }
 
-# Yippieeh, commandline parameters
+status() {
+    local DNS_SD_PID=`getPidDnsSdCommand`
+    if [[ -z "$DNS_SD_PID" ]]; then
+        log "Service dns-sd is not active"
+    else
+        log "Service dns-sd is active"
+    fi
 
-while [ $# -gt 0 ]; do    # Until you run out of parameters . . .
+    local SSH_PID=`getPidSshCommand`
+    if [[ -z "$SSH_PID" ]]; then
+        log "Tunnel to $REMOTE_HOST is not active"
+    else
+        log "Tunnel to $REMOTE_HOST is active"
+    fi
+
+    exit 0
+}
+
+start() {
+    startSshTunnel
+    startDnsSd
+    exit 0
+}
+
+stop() {
+    stopDnsSd
+    stopSshTunnel
+    exit 0
+}
+
+restart() {
+    stopDnsSd
+    stopSshTunnel
+    startSshTunnel
+    startDnsSd
+    exit 0
+}
+
+check() {
+    local SSH_PID=`getPidSshCommand`
+    if [[ -z "$SSH_PID" ]]; then
+        stopDnsSd
+        startSshTunnel
+        startDnsSd
+    else
+        local DNS_SD_PID=`getPidDnsSdCommand`
+        if [[ -z "$DNS_SD_PID" ]]; then
+            startDnsSd
+        else
+            log "Service already active"
+        fi
+    fi
+
+    exit 0
+}
+
+######### End public methods #########
+
+# Until you run out of parameters...
+while [ $# -gt 0 ]; do
     case "$1" in
-        -s|--status)
-            status
-        ;;
-        -k|--kill)
-            killTunnel
-            exit 0
-        ;;
         -q|--quiet)
             QUIET=true
         ;;
         -h|--help)
             help
         ;;
+        -s|--status)
+            status
+        ;;
+        -k|--kill)
+            stop
+        ;;
         -r|--restart)
-            killTunnel
+            restart
+        ;;
+        -c|--check)
+            check
         ;;
         *)
- 
+            help
         ;;
     esac
-    shift       # Check next set of parameters.
+    shift
 done
 
-MYPID=`getPid`
-if [[ -z "$MYPID" ]]; then
-    createTunnel
-else
-    if [ "$QUIET" = "false" ]; then echo "Tunnel to $REMOTE_HOST is already active"; fi
-fi
+start
